3b5bb7edc637f8dd8855bc5cb679bfa99c52462d
Balázs Albert <albertbalzs@gmail.com>
2022-09-16 21:07:19 +0200

    cards style has been changed

--- client/src/pages/Drivers.tsx
+++ client/src/pages/Drivers.tsx
@@ -2,9 +2,9 @@ import { useEffect, useState } from "react";
 import { Container } from "react-bootstrap";
 import axios from "axios";
 import styled from "styled-components";
-import { bindActionCreators } from 'redux';
-import { useDispatch, useSelector } from 'react-redux';
-import { actionCreators, State } from '../state';
+import { bindActionCreators } from "redux";
+import { useDispatch, useSelector } from "react-redux";
+import { actionCreators, State } from "../state";
 // import { Link } from "react-router-dom";
 
 export interface Driver {
@@ -24,28 +24,47 @@ const CardsContainer = styled.div`
   flex-direction: column;
 `;
 
-const Card = styled.div`
-  height: 35rem;
-  width: 30%;
+const CardLeft = styled.div`
+  height: 8rem;
+  width: 50%;
   box-shadow: 0.1rem 0.1rem 1rem rgba(0, 0, 0, 0.2);
-  padding: 2rem;
+  padding: 1rem;
   border-radius: 2rem;
   margin-right: 2rem;
   margin-bottom: 3rem;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+`;
+
+const CardRight = styled.div`
+  height: 8rem;
+  width: 50%;
+  box-shadow: 0.1rem 0.1rem 1rem rgba(0, 0, 0, 0.2);
+  padding: 1rem;
+  border-radius: 2rem;
+  margin-right: 2rem;
+  margin-bottom: 3rem;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+
+  margin-left: 648px;
 `;
 
 const OverTakeButton = styled.button`
-width: auto;
+  width: auto;
+  border-radius: 2rem;
 `;
 
 const Image = styled.img`
   width: auto;
-  height: 300px;
+  height: 100px;
   border-radius: 2rem;
 `;
 
 const Header = styled.h2`
-  margin-top: 1rem;
+  margin-top: 2rem;
   font-size: 1.5rem;
 `;
 
@@ -67,7 +86,7 @@ const ErrorHeader = styled.h2`
   font-size: 3rem;
 `;
 
-const Content = styled.p``;
+// const Content = styled.p``;
 
 const Drivers = () => {
   const [drivers, setArticles] = useState<Driver[]>([]);
@@ -77,15 +96,22 @@ const Drivers = () => {
   }, []);
 
   const fetchDrivers = async () => {
+<<<<<<< Updated upstream
     const { data: response } = await axios.get(
       "http://localhost:8080/drivers"
     );
       console.log(response);
+=======
+    const { data: response } = await axios.get("http://localhost:8080/drivers");
+>>>>>>> Stashed changes
     setArticles(response);
   };
 
   const overTakeDriver = async (driverId: number) => {
+<<<<<<< Updated upstream
     console.log(overTake);
+=======
+>>>>>>> Stashed changes
     const { data: response } = await axios.post(
       `http://localhost:8080/drivers/${driverId}/overtake`
     );
@@ -93,31 +119,63 @@ const Drivers = () => {
     setArticles(response);
   };
 
-
   const dispatch = useDispatch();
 
+<<<<<<< Updated upstream
   const {overTake} = bindActionCreators(actionCreators, dispatch);
   
   const driversInNewPosition = useSelector((state: State) => state.drivers);
 
   console.log(driversInNewPosition);
   
+=======
+  bindActionCreators(actionCreators, dispatch);
+
+  useSelector((state: State) => state.drivers);
+
+>>>>>>> Stashed changes
   return (
     <Container>
       {drivers.length ? (
         <CardsContainer>
-          {drivers.map((driver) => (
-            <Card key={driver.id}>
-              <Image src={`http://localhost:8080${driver.imageUrl}`} />
-              <Header>
-                {driver.firstname} {driver.lastname}
-              </Header>
-              <Content>{driver.country}</Content>
-              <Content>{driver.team}</Content>
-              <Content>{driver.place}</Content>
-              <OverTakeButton onClick={async () => await overTakeDriver(driver.id)} >Overtake</OverTakeButton>
-            </Card>
-          ))}
+          {drivers.map((driver, index) => {
+            if (index % 2 !== 0) {
+              console.log(index);
+              return (
+                <CardRight key={driver.id}>
+                  <Image src={`http://localhost:8080${driver.imageUrl}`} />
+                  <Header>
+                    {driver.firstname} {driver.lastname}
+                  </Header>
+                  <Header>{driver.country}</Header>
+                  <Header>{driver.team}</Header>
+                  <Header>{driver.place}</Header>
+                  <OverTakeButton
+                    onClick={async () => await overTakeDriver(driver.id)}
+                  >
+                    Overtake
+                  </OverTakeButton>
+                </CardRight>
+              );
+            } else {
+              return (
+                <CardLeft key={driver.id}>
+                  <Image src={`http://localhost:8080${driver.imageUrl}`} />
+                  <Header>
+                    {driver.firstname} {driver.lastname}
+                  </Header>
+                  <Header>{driver.country}</Header>
+                  <Header>{driver.team}</Header>
+                  <Header>{driver.place}</Header>
+                  <OverTakeButton
+                    onClick={async () => await overTakeDriver(driver.id)}
+                  >
+                    Overtake
+                  </OverTakeButton>
+                </CardLeft>
+              );
+            }
+          })}
         </CardsContainer>
       ) : (
         <NoDriversContainer>
