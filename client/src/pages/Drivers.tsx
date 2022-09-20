b1cd4aed7d0bc03df317d8fdf3b223ab94b8dcad
Balázs Albert <albertbalzs@gmail.com>
2022-09-20 20:30:25 +0200

    background image

--- client/src/pages/Drivers.tsx
+++ client/src/pages/Drivers.tsx
@@ -18,8 +18,15 @@ export interface Driver {
   place: number;
 }
 
+const backgroundImage = `url("http://localhost:8080/static/foruma-one-background.jpg")`;
+
+const PageBackground = styled.div`
+background-image: ${backgroundImage};
+background-repeat: no-repeat;
+background-size: contain;
+`
 const CardsContainer = styled.div`
-  padding: 4rem 0;
+  padding: 10px;
   display: flex;
   flex-direction: column;
 `;
@@ -31,7 +38,7 @@ const CardLeft = styled(motion.div)`
   padding: 1rem;
   border-radius: 2rem;
   margin-right: 2rem;
-  margin-bottom: 3rem;
+  margin-bottom: 1rem;
   display: flex;
   flex-direction: row;
   justify-content: space-around;
@@ -44,7 +51,7 @@ const CardRight = styled(motion.div)`
   padding: 1rem;
   border-radius: 2rem;
   margin-right: 2rem;
-  margin-bottom: 3rem;
+  margin-bottom: 1rem;
   display: flex;
   flex-direction: row;
   justify-content: space-around;
@@ -106,6 +113,19 @@ const Drivers = () => {
     setArticles(response);
   };
 
+
+  const buttonDisplay = (place: number, id: number) => {
+    if(place > 0) {
+      return (
+        <OverTakeButton
+        onClick={async () => await overTakeDriver(id)}
+      >
+        Overtake
+      </OverTakeButton>
+      )
+    }
+  }
+
   const dispatch = useDispatch();
 
   bindActionCreators(actionCreators, dispatch);
@@ -117,6 +137,7 @@ const Drivers = () => {
   useSelector((state: State) => state.drivers);
 
   return (
+    <PageBackground>      
     <Container>
       {drivers.length ? (
         <CardsContainer>
@@ -124,46 +145,41 @@ const Drivers = () => {
             if (index % 2 !== 0) {
               console.log(index);
               return (
-                  <CardRight
-                  whileHover={{ scale: 1.2 }} 
-                  initial={{opacity: 0}}
-                  animate={{opacity: 1}}
-                  transition={{delay: 0.3, duration: 0.5}}
-                  key={driver.id}>
-                    <Image src={`http://localhost:8080${driver.imageUrl}`} />
-                    <Header>
-                      {driver.firstname} {driver.lastname}
-                    </Header>
-                    <Header>{driver.country}</Header>
-                    <Header>{driver.team}</Header>
-                    <Header>{driver.place}</Header>
-                    <OverTakeButton
-                      onClick={async () => await overTakeDriver(driver.id)}
-                    >
-                      Overtake
-                    </OverTakeButton>
-                  </CardRight>
-              );
-            } else {
-              return (
-                <CardLeft  
-                whileHover={{ scale: 1.2 }} 
-                initial={{opacity: 0}}
-                animate={{opacity: 1}}
-                transition={{delay: 0.3, duration: 0.5}}
-                key={driver.id}>
+                <CardRight
+                  whileHover={{ scale: 1.2 }}
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ delay: 0.3, duration: 0.5 }}
+                  key={driver.id}
+                >
+                  <Header>{driver.place}</Header>
                   <Image src={`http://localhost:8080${driver.imageUrl}`} />
                   <Header>
                     {driver.firstname} {driver.lastname}
                   </Header>
                   <Header>{driver.country}</Header>
                   <Header>{driver.team}</Header>
+                  
+                {buttonDisplay(index, driver.id)}
+                </CardRight>
+              );
+            } else {
+              return (
+                <CardLeft
+                  whileHover={{ scale: 1.2 }}
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ delay: 0.3, duration: 0.5 }}
+                  key={driver.id}
+                >
                   <Header>{driver.place}</Header>
-                  <OverTakeButton
-                    onClick={async () => await overTakeDriver(driver.id)}
-                  >
-                    Overtake
-                  </OverTakeButton>
+                  <Image src={`http://localhost:8080${driver.imageUrl}`} />
+                  <Header>
+                    {driver.firstname} {driver.lastname}
+                  </Header>
+                  <Header>{driver.country}</Header>
+                  <Header>{driver.team}</Header>
+                  {buttonDisplay(index, driver.id)}
                 </CardLeft>
               );
             }
@@ -175,6 +191,7 @@ const Drivers = () => {
         </NoDriversContainer>
       )}
     </Container>
+    </PageBackground>
   );
 };
 
